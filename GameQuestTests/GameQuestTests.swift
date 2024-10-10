3fd951f54aafd512c8ce48a019433ffb65ddf6d3
Usama Waked <w0800780@myscc.ca>
2024-10-10 12:22:34 -0400

    initial commit

---
+++ GameQuestTests/GameQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameQuestTests.swift
+//  GameQuestTests
+//
+//  Created by Usama Waked on 2024-10-10.
+//
+
+import Testing
+@testable import GameQuest
+
+struct GameQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
